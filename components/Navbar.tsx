fa68a24f7ebeb625f208130f21ee93884f0273fd
Dsackler <davidesackler@gmail.com>
2023-05-30 16:39:34 -0700

    finish navbar. Added hidden functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-// import { Link } from 'react-scroll/modules';
+import { Link } from 'react-scroll/modules';
 import { useTheme } from 'next-themes';
 import { RiMoonFill, RiSunLine } from 'react-icons/ri';
 import { IoMdMenu, IoMdClose } from 'react-icons/io';
@@ -34,32 +34,63 @@ export const Navbar = () => {
     <nav className='w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow dark:border-b dark:border-stone-600'>
       <div className='justify-between md:items-center md:flex'>
         <div>
-          <div className='md:py-5 md:block'>
-            <h2 className='text-2xl font-bold'>David Sackler</h2>
+          <div className='flex items-center justify-between py-3'>
+            <div className='md:py-5 md:block'>
+              <h2 className='text-2xl font-bold'>David Sackler</h2>
+            </div>
+            <div className='md:hidden'>
+              <button onClick={() => setNavBar(!navBar)}>
+                {navBar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-        <div className='items-center justify-center md:flex md:space-x-6'>
-          <>
-            {NAV_ITEMS.map((item, idx) => {
-              return <a key={idx}>{item.label}</a>;
-            })}
-            {/* themes work only if theme provider is wrapped around the component in layout */}
-            {currentTheme === 'dark' ? (
-              <button
-                onClick={() => setTheme('light')}
-                className='bg-slate-100 p-2 rounded-xl'
-              >
-                <RiSunLine size={25} color='black' />
-              </button>
-            ) : (
-              <button
-                onClick={() => setTheme('dark')}
-                className='bg-slate-100 p-2 rounded-xl'
-              >
-                <RiMoonFill size={25} color='black' />
-              </button>
-            )}
-          </>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:m-0 ${
+              navBar ? 'block' : 'hidden'
+            }`}
+          >
+            <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
+              <>
+                {NAV_ITEMS.map((item, idx) => {
+                  return (
+                    <Link
+                      key={idx}
+                      to={item.page}
+                      className={
+                        'block lg:inline-block hover:text-neutral-500 cursor-pointer'
+                      }
+                      activeClass='active'
+                      spy={true}
+                      smooth={true}
+                      offset={-100}
+                      duration={500}
+                      onClick={() => setNavBar(!navBar)}
+                    >
+                      {item.label}
+                    </Link>
+                  );
+                })}
+                {/* themes work only if theme provider is wrapped around the component in layout */}
+                {currentTheme === 'dark' ? (
+                  <button
+                    onClick={() => setTheme('light')}
+                    className='bg-slate-100 p-2 rounded-xl'
+                  >
+                    <RiSunLine size={25} color='black' />
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => setTheme('dark')}
+                    className='bg-slate-100 p-2 rounded-xl'
+                  >
+                    <RiMoonFill size={25} color='black' />
+                  </button>
+                )}
+              </>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
