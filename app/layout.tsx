cdc544a0e9e4597f4fbf816c242dbd0b7ebfcc5c
Dsackler <davidesackler@gmail.com>
2023-05-29 22:58:56 -0700

    added nav functionality and color modes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import './globals.css'
-import { Navbar } from '@/components/Navbar'
-
-
+'use client';
+import './globals.css';
+import { Navbar } from '@/components/Navbar';
+import { ThemeProvider } from 'next-themes';
 
 export const metadata = {
   title: 'David Sackler',
   description: 'My Portfolio (A portion of it at least)',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <Navbar />
-        {children}
-        </body>
+        <ThemeProvider enableSystem={true} attribute='class'>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,67 @@
-'use client'
-import React, { useState } from 'react'
-import { Link } from 'react-scroll/modules'
-import { useTheme } from 'next-themes'
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
+'use client';
+import React, { useState } from 'react';
+// import { Link } from 'react-scroll/modules';
+import { useTheme } from 'next-themes';
+import { RiMoonFill, RiSunLine } from 'react-icons/ri';
+import { IoMdMenu, IoMdClose } from 'react-icons/io';
 
 interface NavItem {
-    label: string,
-    page: string
+  label: string;
+  page: string;
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-    {
-        label: 'Home',
-        page: 'home'
-    },
-    {
-        label: 'About',
-        page: 'about'
-    },
-    {
-        label: 'Projects',
-        page: 'projects'
-    }
-]
+  {
+    label: 'Home',
+    page: 'home',
+  },
+  {
+    label: 'About',
+    page: 'about',
+  },
+  {
+    label: 'Projects',
+    page: 'projects',
+  },
+];
 
 export const Navbar = () => {
-    const {systemTheme, theme, setTheme} = useTheme()
-    const currentTheme = theme === 'system' ? systemTheme : theme
-    const [navBar, setNavBar] = useState(false)
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const [navBar, setNavBar] = useState(false);
 
   return (
-    <header>
-        <div className='justify-between md:items-center md:flex'>
-            <div>
-                <div>
-                    <h2>David Sackler</h2>
-                </div>
-            </div>
-            <div>
-                {NAV_ITEMS.map((item, idx) => {
-                    return <a key = {idx}>{item.label}</a>
-                })}
-            </div>
+    <nav className='w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow dark:border-b dark:border-stone-600'>
+      <div className='justify-between md:items-center md:flex'>
+        <div>
+          <div className='md:py-5 md:block'>
+            <h2 className='text-2xl font-bold'>David Sackler</h2>
+          </div>
         </div>
-    </header>
-  )
-}
+        <div className='items-center justify-center md:flex md:space-x-6'>
+          <>
+            {NAV_ITEMS.map((item, idx) => {
+              return <a key={idx}>{item.label}</a>;
+            })}
+            {/* themes work only if theme provider is wrapped around the component in layout */}
+            {currentTheme === 'dark' ? (
+              <button
+                onClick={() => setTheme('light')}
+                className='bg-slate-100 p-2 rounded-xl'
+              >
+                <RiSunLine size={25} color='black' />
+              </button>
+            ) : (
+              <button
+                onClick={() => setTheme('dark')}
+                className='bg-slate-100 p-2 rounded-xl'
+              >
+                <RiMoonFill size={25} color='black' />
+              </button>
+            )}
+          </>
+        </div>
+      </div>
+    </nav>
+  );
+};
