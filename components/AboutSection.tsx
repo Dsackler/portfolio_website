6b5bd0b447bba9c527e6c9f49b45f2c1de696057
Dsackler <davidesackler@gmail.com>
2023-05-30 18:18:11 -0700

    added about section. Working on styling

---
+++ components/AboutSection.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Image from 'next/image';
+
+const skills = [
+  { skill: 'HTML' },
+  { skill: 'CSS' },
+  { skill: 'JavaScript' },
+  { skill: 'TypeScript' },
+  { skill: 'Python' },
+  { skill: 'React' },
+  { skill: 'Next.js' },
+  { skill: 'Tailwind CSS' },
+  { skill: 'Git' },
+  { skill: 'GitHub' },
+  { skill: 'Jupyter Notebooks' },
+];
+
+export const AboutSection = () => {
+  return (
+    <section id='about'>
+      <div className='my-12 pb-12 md:pt-16 md:pb-48'>
+        <h1 className='text-center font-bold text-4xl'>About Me</h1>
+        <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10'>
+          <div className='md: w-1/2'>
+            <h1 className='text-center text-2xl font-bold mb-6 md:text-left'>
+              Get to know me!
+            </h1>
+            <p>
+              I work well in both
+              <span className='font-bold'> group collaborative</span> and
+              <span className='font-bold'> independent settings</span>. In small
+              groups, I excel at contributing technical solutions and
+              effectively communicate with my team to overcome technical
+              challenges. I am a confident public speaker and I have the ability
+              to communicate my ideas both verbally and in writing. When given a
+              problem to solve, I look for creative solutions and work
+              diligently until the problem is resolved.
+            </p>
+          </div>
+          <div className='md:w-1/2'>
+            <h1 className='text-2xl font-bold mb-6'>My Skills</h1>
+            <div className='flex flex-wrap flex-row justify center md: justify-start'>
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className='bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold'
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+            <Image
+              src='/Headphone Holder.jpg'
+              alt=''
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
