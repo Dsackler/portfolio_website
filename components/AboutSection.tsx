71d217f3804a403c48fcc4d820586352f8958593
Dsackler <davidesackler@gmail.com>
2023-05-30 18:32:31 -0700

    added about image and hr

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -19,7 +19,10 @@ export const AboutSection = () => {
   return (
     <section id='about'>
       <div className='my-12 pb-12 md:pt-16 md:pb-48'>
-        <h1 className='text-center font-bold text-4xl'>About Me</h1>
+        <h1 className='text-center font-bold text-4xl'>
+          About Me
+          <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
+        </h1>
         <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10'>
           <div className='md: w-1/2'>
             <h1 className='text-center text-2xl font-bold mb-6 md:text-left'>
@@ -39,7 +42,7 @@ export const AboutSection = () => {
           </div>
           <div className='md:w-1/2'>
             <h1 className='text-2xl font-bold mb-6'>My Skills</h1>
-            <div className='flex flex-wrap flex-row justify center md: justify-start'>
+            <div className='flex flex-wrap flex-row justify center md: justify-start pb-4'>
               {skills.map((item, idx) => {
                 return (
                   <p
@@ -52,10 +55,11 @@ export const AboutSection = () => {
               })}
             </div>
             <Image
-              src='/Headphone Holder.jpg'
+              className='hidden md:block md:relative md:bottom-4 md:left-32 md:z-0'
+              src='/Programmer-image.jpg'
               alt=''
-              width={100}
-              height={100}
+              width={325}
+              height={325}
             />
           </div>
         </div>
