26831cc6831cab02248631a5701101b819a44083
Dsackler <davidesackler@gmail.com>
2023-06-07 15:11:21 -0700

    temporarily disabled the metadata export

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'David',
-  description: 'My Portfolio (A portion of it at least)',
-};
+// export const metadata = {
+//   title: 'David',
+//   description: 'My Portfolio (A portion of it at least)',
+// };
 
 export default function RootLayout({
   children,
